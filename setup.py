3fe0f35475fa320a45c6f71b7524fe0fdaa71f1d
Sebastian <sebsatianbod@gmail.com>
2019-10-09 12:50:03 +0200

    Add setup file to make package pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='blinker',
+      version='0.1',
+      description='OpenAI Gym Wrappers for introducing active, partial observability.',
+      url='https://github.com/rluct/blinker',
+      author='UCT RL',
+      author_email='sebastianbod@gmail.com',
+      license='MIT',
+      packages=['blinker'],
+      zip_safe=False)
